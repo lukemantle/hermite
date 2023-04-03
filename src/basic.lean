1cc0886b84e25ae9392375d30502d2390aa6fb34
lukemantle <lukeomantle@gmail.com>
2023-04-03 11:17:20 -0700

    documentation on basic.lean

--- src/basic.lean
+++ src/basic.lean
@@ -1,10 +1,33 @@
-import analysis.calculus.mean_value
-open polynomial
+/-
+Copyright (c) 2023 Luke Mantle. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Luke Mantle.
+-/
 
+import analysis.calculus.deriv
+open polynomial
 open set filter
+
+/-!
+# Hermite polynomials
+
+This file defines `hermite n`, the nth probabilist's Hermite polynomial.
+
+## Main definitions
+
+* `x_sub_dx`  : the operation `(x - d/dx)` used to recursively define the Hermite polynomials
+* `hermite n` : the nth probabilist's Hermite polynomial, defined as a `polynomial ℝ`, using
+                `x_sub_dx` recursively
+
+## Notation
+
+## References
+
+-/
 
 noncomputable theory
 
+/-- the operation `(x - d/dx)` defined for `polynomial ℝ` -/
 @[simp]
 def x_sub_dx (p : polynomial ℝ) :=
 X * p - p.derivative
@@ -14,6 +37,7 @@ lemma x_sub_dx_def (p : polynomial ℝ) : x_sub_dx p = X*p - p.derivative := rfl
 lemma x_sub_dx_apply (p : polynomial ℝ) (x : ℝ) :
 eval x (x_sub_dx p) = x*(eval x p) - (eval x (derivative p)) := by simp
 
+/-- the nth probabilist's Hermite polynomial -/
 def hermite (n : ℕ) : polynomial ℝ :=
 nat.iterate x_sub_dx n 1
 
