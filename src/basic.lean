d35d74e2d304c07ff2eab778ce5ad39693bd2c95
Jake Levinson <levinson.jake@gmail.com>
2023-04-03 12:29:32 -0700

    tidying edits to basic.lean and coeff.lean

--- src/basic.lean
+++ src/basic.lean
@@ -1,38 +1,31 @@
-import analysis.calculus.mean_value
-open polynomial
+import data.polynomial.derivative
+import data.real.basic
 
-open set filter
+open polynomial
 
 noncomputable theory
 
-@[simp]
-def x_sub_dx (p : polynomial ℝ) :=
-X * p - p.derivative
+def x_sub_dx (p : polynomial ℝ) := X * p - p.derivative
 
-lemma x_sub_dx_def (p : polynomial ℝ) : x_sub_dx p = X*p - p.derivative := rfl
+@[simp] lemma x_sub_dx_def (p : polynomial ℝ) : x_sub_dx p = X * p - p.derivative := rfl
 
-lemma x_sub_dx_apply (p : polynomial ℝ) (x : ℝ) :
-eval x (x_sub_dx p) = x*(eval x p) - (eval x (derivative p)) := by simp
+@[simp] lemma x_sub_dx_apply (p : polynomial ℝ) (x : ℝ) :
+  eval x (x_sub_dx p) = x * (eval x p) - (eval x (derivative p)) := by simp
 
-def hermite (n : ℕ) : polynomial ℝ :=
-nat.iterate x_sub_dx n 1
+def hermite (n : ℕ) : polynomial ℝ := nat.iterate x_sub_dx n 1
 
-@[simp]
 lemma hermite_eq_iter (n : ℕ) : hermite n = nat.iterate x_sub_dx n 1 := rfl
 
-@[simp]
-lemma hermite_succ (n : ℕ) : hermite n.succ = x_sub_dx (hermite n) :=
+@[simp] lemma hermite_succ (n : ℕ) : hermite n.succ = x_sub_dx (hermite n) :=
 begin
-  simp only [hermite],
-  rw function.iterate_succ' x_sub_dx n,
+  rw [hermite_eq_iter, function.iterate_succ' x_sub_dx n],
+  refl,
 end
 
-@[simp]
-lemma hermite_zero : hermite 0 = C 1 := rfl
+@[simp] lemma hermite_zero : hermite 0 = C 1 := rfl
 
-@[simp]
-lemma hermite_one : hermite 1 = X :=
+@[simp] lemma hermite_one : hermite 1 = X :=
 begin
-  rw [hermite_succ, x_sub_dx_def],
-  simp only [hermite_zero, map_one, mul_one, derivative_one, sub_zero],
+  rw [hermite_succ, x_sub_dx_def, hermite_zero],
+  simp only [map_one, mul_one, derivative_one, sub_zero],
 end
--- src/coeff.lean
+++ src/coeff.lean
@@ -1,49 +1,39 @@
-import analysis.calculus.mean_value
-import data.nat.factorial.basic
-import data.nat.choose.basic
+import data.nat.parity
 import basic
-import exp
-open polynomial
 
-open set filter
+open polynomial
 
 noncomputable theory
 
-lemma hermite_zero_zero : coeff (hermite 0) 0 = 1 := coeff_one_zero
-
-lemma hermite_one_zero : coeff (hermite 1) 0 = 0 :=
-begin
-  rw hermite_one,
-  exact coeff_X_zero,
-end
-
-lemma hermite_one_one : coeff (hermite 1) 1 = 1 :=
-begin
-  rw hermite_one,
-  exact coeff_X_one,
-end
+@[simp] lemma hermite_zero_zero : coeff (hermite 0) 0 = 1 := coeff_one_zero
+@[simp] lemma hermite_one_zero : coeff (hermite 1) 0 = 0 := by rw [hermite_one, coeff_X_zero]
+@[simp] lemma hermite_one_one : coeff (hermite 1) 1 = 1 := by rw [hermite_one, coeff_X_one]
 
 lemma x_sub_dx_coeff (p : polynomial ℝ) (k : ℕ) :
-coeff (x_sub_dx p) k = coeff (X*p) k - coeff (p.derivative) k := by simp
+  coeff (x_sub_dx p) k = coeff (X * p) k - coeff (p.derivative) k := by simp
 
-lemma hermite_coeff_recur (n k : ℕ) :
-coeff (hermite (n + 1)) (k + 1) = coeff (hermite n) k - (k+2)*(coeff (hermite n) (k+2)) :=
+lemma hermite_coeff_recur_zero (n : ℕ) :
+  coeff (hermite (n + 1)) 0 = -(coeff (hermite n) 1) :=
 begin
-  rw [hermite_succ, x_sub_dx_coeff, coeff_X_mul, coeff_derivative, mul_comm],
-  suffices : ((k+1 : ℕ) : ℝ) + 1 = k + 2,
-  { rw this, },
-  { simp only [nat.cast_add, nat.cast_one],
-    ring, },
+  rw [hermite_succ, x_sub_dx_coeff, coeff_X_mul_zero, coeff_derivative],
+  norm_num,
 end
 
-lemma hermite_coeff_upper (n k : ℕ) : coeff (hermite n) (n+k+1) = 0 :=
+lemma hermite_coeff_recur (n k : ℕ) :
+  coeff (hermite (n + 1)) (k + 1) = coeff (hermite n) k - (k + 2) * (coeff (hermite n) (k + 2)) :=
+begin
+  rw [hermite_succ, x_sub_dx_coeff, coeff_X_mul, coeff_derivative, mul_comm],
+  norm_cast,
+end
+
+lemma hermite_coeff_upper (n k : ℕ) : coeff (hermite n) (n + k + 1) = 0 :=
 begin
   induction n with n ih generalizing k,
   { apply coeff_C },
   {  rw [hermite_coeff_recur,
       (by linarith : n + 1 + k = n + k + 1),
       (by linarith : n + k + 1 + 2 = n + (k + 2) + 1),
-      ih k, ih (k+2), mul_zero, sub_zero] }
+      ih k, ih (k + 2), mul_zero, sub_zero] }
 end
 
 lemma hermite_coeff_upper' (n k : ℕ) (hnk : n < k) : coeff (hermite n) k = 0 :=
@@ -59,30 +49,15 @@ begin
   { rw [hermite_coeff_recur, ih, hermite_coeff_upper, mul_zero, sub_zero] }
 end
 
-lemma hermite_coeff_recur_zero (n : ℕ) :
-coeff (hermite (n + 1)) 0 = -(coeff (hermite n) 1) :=
-begin
-  rw [hermite_succ, x_sub_dx_def, coeff_sub, coeff_X_mul_zero, coeff_derivative, zero_sub, zero_add],
-  norm_num
-end
-
-lemma hermite_coeff_odd_zero (n k : ℕ) : odd (n + k) → coeff (hermite n) k = 0 :=
+lemma hermite_coeff_odd_zero (n k : ℕ) (hnk : odd (n + k)) : coeff (hermite n) k = 0 :=
 begin
   induction n with n ih generalizing k,
-  { intro h,
-    rw zero_add at h,
-    apply hermite_coeff_upper',
-    cases k,
-    { exfalso,
-      exact nat.even_iff_not_odd.mp (even_zero) h },
-    { exact ne_zero.pos (nat.succ k) } },
-  { intro h,
-    induction k with k ihk,
-    { rw hermite_coeff_recur_zero,
-      rw [ih 1 h, neg_zero] },
-    { rw [hermite_coeff_recur, ih k, ih (k+2)],
-      { ring },
-      { rwa (by simp [nat.succ_add, nat.add_succ] : n.succ + k.succ = n + (k + 2)) at h },
-      { rw (by simp [nat.succ_add, nat.add_succ] : n.succ + k.succ = n + k + 2) at h,
-        exact (nat.odd_add.mp h).mpr even_two }}}
+  { rw zero_add at hnk,
+    exact hermite_coeff_upper' _ _ hnk.pos },
+  { cases k,
+    { rw [hermite_coeff_recur_zero, ih 1 hnk, neg_zero] },
+    { rw [hermite_coeff_recur, ih k _, ih (k + 2) _, mul_zero, sub_zero],
+      { rwa (by simp [nat.succ_add, nat.add_succ] : n.succ + k.succ = n + (k + 2)) at hnk },
+      { rw (by simp [nat.succ_add, nat.add_succ] : n.succ + k.succ = n + k + 2) at hnk,
+        exact (nat.odd_add.mp hnk).mpr even_two }}}
 end
