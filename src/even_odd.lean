2e857f22b171febadbed9a9822e64c4b11446b04
Jake Levinson <levinson.jake@gmail.com>
2023-04-04 15:08:18 -0700

    even and odd polynomials

--- src/even_odd.lean
+++ src/even_odd.lean
@@ -42,3 +42,27 @@ end
 --   have a := p.support,
 --   have h : p = (∑ n in p.support, coeff p n),
 -- end
+
+variables {R : Type} [comm_ring R] (p : polynomial R)
+
+lemma polynomial_even (hp : ∀ n, odd n → p.coeff n = 0) (x) : p.eval (-x) = p.eval x :=
+begin
+  repeat {rw polynomial.eval_eq_sum_range},
+  congr,
+  ext i,
+  cases i.even_or_odd with hi hi,
+  { rw [neg_pow, hi.neg_one_pow, one_mul] },
+  { rw [hp _ hi, zero_mul, zero_mul] },
+end
+
+lemma polynomial_odd (hp : ∀ n, even n → p.coeff n = 0) (x) : p.eval (-x) = -(p.eval x) :=
+begin
+  repeat {rw polynomial.eval_eq_sum_range},
+  nth_rewrite 0 (neg_eq_neg_one_mul _),
+  rw [finset.mul_sum],
+  congr,
+  ext i,
+  cases i.even_or_odd with hi hi,
+  { rw hp _ hi, ring },
+  { rw [neg_pow, hi.neg_one_pow], ring },
+end
