ea1f3840a7d4b12645a967ed7aa11ca019a36be3
lukemantle <lukeomantle@gmail.com>
2023-04-07 13:12:57 -0700

    remove x_sub_dx from basic.lean

--- src/basic.lean
+++ src/basic.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Luke Mantle. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Luke Mantle.
+Authors: Luke Mantle
 -/
 
 import data.polynomial.derivative
@@ -26,40 +26,18 @@ This file defines `hermite n`, the nth probabilist's Hermite polynomial.
 
 noncomputable theory
 
-section x_sub_dx
-
-variables {R : Type} [comm_ring R] (p : polynomial R)
-
-/-- the operation `(x - d/dx)` defined for `polynomial R` -/
-def x_sub_dx := X * p - p.derivative
-
-@[simp] lemma x_sub_dx_def : x_sub_dx p = X * p - p.derivative := rfl
-
-lemma x_sub_dx_apply {x : R} :
-  eval x (x_sub_dx p) = x * (eval x p) - (eval x (derivative p)) := by simp
-
-@[simp] lemma x_sub_dx_map {S : Type} [comm_ring S] {f : R →+* S} :
-  x_sub_dx (map f p) = map f (x_sub_dx p) := by simp
-
-end x_sub_dx
-
-section hermite
-
-#check (X : polynomial ℤ).derivative
-
 /-- the nth probabilist's Hermite polynomial -/
 noncomputable def hermite : ℕ → polynomial ℤ
 | 0     := 1
-| (n+1) := x_sub_dx (hermite n)
+| (n+1) := X * (hermite n) - (hermite n).derivative
 
-@[simp] lemma hermite_succ {n : ℕ} : hermite n.succ = x_sub_dx (hermite n) := by rw hermite
+@[simp] lemma hermite_succ {n : ℕ} : hermite (n+1) =  X * (hermite n) - (hermite n).derivative :=
+by rw hermite
 
 @[simp] lemma hermite_zero : hermite 0 = C 1 := rfl
 
 @[simp] lemma hermite_one : hermite 1 = X :=
 begin
-  rw [hermite_succ, x_sub_dx_def, hermite_zero],
-  simp only [map_one, mul_one, derivative_one, sub_zero],
+  rw [hermite_succ, hermite_zero],
+  simp only [map_one, mul_one, derivative_one, sub_zero]
 end
-
-end hermite
